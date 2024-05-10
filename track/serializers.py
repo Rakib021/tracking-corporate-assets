b4700285ca8eb8a455e110ef9575c18588651400
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-05-10 18:45:57 +0600

    track-corporate-assets-api

---
+++ track/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import *
+
+# serialisers to clean and formate data
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    employe = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+class AssetsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssetsModel
+        fields = '__all__'
+
+
+class AssetsLogPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssetsLog
+        fields = '__all__'
+
+
+class AssetsLogGetSerializer(serializers.ModelSerializer):
+    asset = serializers.StringRelatedField()
+    employee = serializers.StringRelatedField()
+
+    class Meta:
+        model = AssetsLog
+        fields = '__all__'
